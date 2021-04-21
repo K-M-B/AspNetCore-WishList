f8cc1010132bf2a2aef39dba7210c409e7ef5ec7
kb-bluekey <karen@bluekeyinc.com>
2021-04-21 09:36:36 -0400

    Completed module 3

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -18,7 +18,7 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddControllersWithViews();
+            //services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
